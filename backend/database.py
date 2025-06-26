82f309b50bc82d138f73f6076f0cd5104c6c73a3
RhyanCodes <dabberino@gmail.com>
2025-06-26 14:38:37 -0400

    new password

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,10 @@
+# imports create_engine function from sqlalchemy library
 from sqlalchemy import create_engine
+# import declarative base_function 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Clutch95$@localhost/njtsmartparking"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:FF7Cloud$@localhost/njtsmartparking"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
